f8fe01367c7659f91c7769bcaedbfcdb90a9dba8
Pavel <ppkonstantinovch@gmail.com>
2022-07-03 19:41:16 +0300

    Появилась новая ошибка TypeError: __init__() got an unexpected keyword argument 'follow_redirect' не монимаю.....

--- bp_api/views.py
+++ bp_api/views.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
-from flask import Blueprint, jsonify, abort, logging
-
+from flask import Blueprint, jsonify, abort
+import logging
 from bp_posts.dao.comment import Comment
 from bp_posts.dao.post import Post
 from bp_posts.dao.post_dao import PostDAO
@@ -9,7 +9,7 @@ from bp_posts.dao.comment_dao import CommentDAO
 
 from config import DATA_PATH_POSTS, DATA_PATH_COMMENTS
 
-#Создаем блупринт
+# Создаем блупринт
 bp_api = Blueprint("bp_api", __name__)
 
 # Создаем обьекты доступа к данным
@@ -24,26 +24,30 @@ def api_posts_all():
     """Этдпоинт для всех постов"""
     all_posts: list[Post] = post_dao.get_all()
     all_posts_as_dicts: list[dict] = [post.as_dict() for post in all_posts]
+
     api_logger.debug("Запрошены все посты")
+
     return jsonify(all_posts_as_dicts), 200
+
 
 @bp_api.route('/posts/<int:pk>/')
 def api_posts_single(pk: int):
     """Этдпоинт для одного постоа"""
     post: Optional[Post] = post_dao.get_by_pk(pk)
     if post is None:
+        api_logger.debug(f"Обращение к несуществующему посту {pk}")
         abort(404)
-
+    api_logger.debug(f"Запрошен пост {pk}")
     return jsonify(post.as_dict()), 200
+
 
 @bp_api.errorhandler(404)
 def api_error_404(error):
+    api_logger.error(f"Ошибка{error}")
     return jsonify({"error": str(error)}), 404
+
 
 @bp_api.route('/')
 def api_posts_hello():
-
     return "Это фпи. Доступные эндпоинты /api/posts и  /api/posts/<pk>. " \
            "Смотри документацию у меня на гитхабе"
-
-
